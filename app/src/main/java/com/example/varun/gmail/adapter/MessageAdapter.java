7553436e032170f41293492eb6bb1c5345581711
varundm <mailforvarundas@gmail.com>
2018-05-11 23:08:41 +0530

    Set profile picture

--- app/src/main/java/com/example/varun/gmail/adapter/MessageAdapter.java
+++ app/src/main/java/com/example/varun/gmail/adapter/MessageAdapter.java
@@ -2,13 +2,17 @@ package com.example.varun.gmail.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.varun.gmail.R;
+import com.example.varun.gmail.helper.CircleTransform;
 import com.example.varun.gmail.model.Message;
 
 import java.util.List;
@@ -32,8 +36,10 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHo
             from = (TextView) view.findViewById(R.id.from);
             subject = (TextView) view.findViewById(R.id.txt_primary);
             message = (TextView) view.findViewById(R.id.txt_secondary);
+            timestamp = (TextView) view.findViewById(R.id.timestamp);
 
-
+            imageProfile = (ImageView) view.findViewById(R.id.icon_profile);
+            iconText =  (TextView) view.findViewById(R.id.icon_text);
         }
     }
 
@@ -56,8 +62,26 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHo
         holder.from.setText(message.getFrom());
         holder.subject.setText(message.getSubject());
         holder.message.setText(message.getMessage());
+        holder.timestamp.setText(message.getTimestamp());
 
+        holder.iconText.setText(message.getFrom().substring(0,1));
+        applyProfilePicture(holder,message);
+    }
 
+    private void applyProfilePicture(MyViewHolder holder, Message message) {
+        if(!(TextUtils.isEmpty(message.getPicture()))) {
+            Glide.with(mContext).load(message.getPicture())
+                    .thumbnail(.5f)
+                    .crossFade()
+                    .transform(new CircleTransform(mContext))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(holder.imageProfile);
+            holder.imageProfile.setColorFilter(null);
+            holder.iconText.setVisibility(View.GONE);
+        } else {
+            holder.imageProfile.setImageResource(R.drawable.bg_circle);
+            holder.iconText.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
